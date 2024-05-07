453cab5e5373b6aaa98fe71cff48c23e1b5ce6a1
Lesley Ventura <lesleyventura@Lesleys-MacBook-Pro.local>
2024-05-07 17:40:58 -0400

    Initial commit

---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,13 @@
+import java.io.*
+public class SinglyLinkedList {
+
+    private static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
